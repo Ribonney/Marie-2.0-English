d85fe61cb94bcd1a4fe0e040abab192b51b0b29c
Ribonney <57567251+Ribonney@users.noreply.github.com>
2020-11-22 17:30:41 +0300

    Update bans.py

--- tg_bot/modules/bans.py
+++ tg_bot/modules/bans.py
@@ -72,7 +72,7 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
             raise
 
     if is_user_ban_protected(chat, user_id, member):
-        message.reply_text("Yöneticileri Yasaklayabilmeyi Gerçekten Çok İsterdim...")
+        message.reply_text("Gerçekten Sahibimi Banlamamı Mı İstiyorsun ??...")
         return ""
 
     if user_id == bot.id:
