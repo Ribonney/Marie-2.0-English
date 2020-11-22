7a16f162123b8558511a8ca67be68772246e8719
Ribonney <57567251+Ribonney@users.noreply.github.com>
2020-11-22 17:14:40 +0300

    Update bans.py

--- tg_bot/modules/bans.py
+++ tg_bot/modules/bans.py
@@ -91,7 +91,8 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
         chat.kick_member(user_id)
         bot.send_sticker(chat.id, BAN_STICKER)  # banhammer marie sticker
         keyboard = []
-        reply = "{} Banned!".format(mention_html(member.user.id, member.user.first_name))
+        reply = """{} Yasaklandı!
+        Sebep: {}""".format(mention_html(member.user.id, member.user.first_name),reason)
         message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
         return log
 
