e2c3216a053aaf7251bbb02fa0fa16af93709d3d
Ribonney <57567251+Ribonney@users.noreply.github.com>
2020-11-22 18:21:53 +0300

    Update bans.py

--- tg_bot/modules/bans.py
+++ tg_bot/modules/bans.py
@@ -358,7 +358,8 @@ def rban(bot: Bot, update: Update, args: List[str]):
 
     try:
         chat.kick_member(user_id)
-        message.reply_text(f"[kullanıcı](tg://user?id={}) Yasaklandı".format(user_id), parse_mode='Markdown')
+        user = f"[kullanıcı](tg://user?id={})".format(user_id)
+        message.reply_text("{} Yasaklandı".format(user), parse_mode='Markdown')
     except BadRequest as excp:
         if excp.message == "Reply message not found":
             # Do not reply
