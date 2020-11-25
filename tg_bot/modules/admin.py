bcb4933b65b3f89239847ebf8e204adce2e7afb8
Ribonney <57567251+Ribonney@users.noreply.github.com>
2020-11-25 06:30:25 +0300

    Update admin.py

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -44,17 +44,23 @@ def promote(bot: Bot, update: Update, args: List[str]) -> str:
     bot_member = chat.get_member(bot.id)
 
     bot.promoteChatMember(chat_id, user_id,
-                          can_change_info=bot_member.can_change_info,
+                          #can_change_info=bot_member.can_change_info
                           can_post_messages=bot_member.can_post_messages,
                           can_edit_messages=bot_member.can_edit_messages,
                           can_delete_messages=bot_member.can_delete_messages,
                           can_invite_users=bot_member.can_invite_users,
-                          can_restrict_members=bot_member.can_restrict_members,
-                          can_pin_messages=bot_member.can_pin_messages,
+                          #can_restrict_members=bot_member.can_restrict_members,
+                          can_pin_messages=bot_member.can_pin_messages
                           #can_promote_members=bot_member.can_promote_members)
 
     message.reply_text("Yetkilendirildi🧡")
-    
+    return "<b>{}:</b>" \
+           "\n#Yetkilendirildi" \
+           "\n<b>Admin:</b> {}" \
+           "\n<b>Kullanıcı:</b> {}".format(html.escape(chat.title),
+                                      mention_html(user.id, user.first_name),
+                                      mention_html(user_member.user.id, user_member.user.first_name))
+
 
 
 
