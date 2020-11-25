a482c9191864eb93eda97102022c903ce5da729c
Ribonney <57567251+Ribonney@users.noreply.github.com>
2020-11-25 14:04:33 +0300

    Update notes.py

--- tg_bot/modules/notes.py
+++ tg_bot/modules/notes.py
@@ -271,7 +271,7 @@ def __import_data__(chat_id, data):
 
 
 def __stats__():
-    return "{} notes, across {} chats.".format(sql.num_notes(), sql.num_chats())
+    return "{} Sohbette Toplam {} Not Bulunuyor.".format(sql.num_chats(), sql.num_notes())
 
 
 def __migrate__(old_chat_id, new_chat_id):
